dfb011dbfac5323ea25c08423070336c4b7083f9
AliNouman777 <hb6348248@gmail.com>
2024-08-23 16:48:23 +0500

    fixing port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3001,  // Use the port provided by Render or default to 3000
+    strictPort: true,                // Fail if the port is already in use
+    host: '0.0.0.0'                  // Bind to all IP addresses, allowing external access
+  }
 })
