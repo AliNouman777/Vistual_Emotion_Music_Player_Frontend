e7da9cdb625d9283dc8c10fbdcb0d82d66f28f5f
AliNouman777 <hb6348248@gmail.com>
2024-08-24 14:55:47 +0500

    fixing home page ui

--- src/Features/Home/WebCamCapture/Home.jsx
+++ src/Features/Home/WebCamCapture/Home.jsx
@@ -5,9 +5,9 @@ import img from "../home.png";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-const WebCamCapture = () => {
-  const Navigate = useNavigate();
- 
+const Home = () => {
+  const navigate = useNavigate();
+
   const imageAnimation = {
     y: [0, -20, 0],
     transition: {
@@ -18,32 +18,36 @@ const WebCamCapture = () => {
     },
   };
 
-  const navbtn=()=>{
-    Navigate("/capture")
-  }
+  const handleNavigation = () => {
+    navigate("/capture");
+  };
 
   return (
     <>
-      <div className="pcon">
-        <div className="container">
-          <div className="container">
-            <div className="textcon typing">
-              <h1>Visual Emotion Base Music Player</h1>
-              <p className="homePara">
-              Discover music tailored to your emotions. Visual Emotion Base Music Player uses advanced AI to analyze your facial expressions and curate a personalized soundtrack. Let your feelings guide the rhythm, as we transform your mood into melodies. Experience music like never before.
-              </p>
-            </div>
+      <div className="page-container">
+        <div className="content-container">
+          <div className="text-container typing">
+            <h1>Visual Emotion Based Music Player</h1>
+            <p className="home-paragraph">
+              Discover music tailored to your emotions. Visual Emotion Based
+              Music Player uses advanced AI to analyze your facial expressions
+              and curate a personalized soundtrack. Let your feelings guide the
+              rhythm, as we transform your mood into melodies. Experience music
+              like never before.
+            </p>
           </div>
-          <div className="imgcon">
+          <div className="image-container">
             <motion.img src={img} alt="music" animate={imageAnimation} />
           </div>
         </div>
-        <div className="start">
-        <button onClick={navbtn} className="startbtn"><Link className="btnlink">Let's Start</Link></button>
+        <div className="start-button-container">
+          <button onClick={handleNavigation} className="start-button">
+            <Link className="button-link">Let's Start</Link>
+          </button>
         </div>
       </div>
     </>
   );
 };
 
-export default WebCamCapture;
+export default Home;
